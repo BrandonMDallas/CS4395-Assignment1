b2f15dab96ea2debab78cbd268bcbf079a8d5e50
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-25 15:12:04 -0600

    Moving over the data class

--- assignment1_code.py
+++ assignment1_code.py
@@ -1,5 +1,33 @@
 import math
+import numpy as np
+import pandas as pd
+import nltk
 
+"""
+Used for loading in the train and validation data in google drive
+"""
+class Data():
+  def __init__(self):
+    drive.mount('/content/drive')
+
+  def get_train(self):
+    train_path = '/content/drive/My Drive/CS_4395_Assignments/A1/train.txt'
+    with open(train_path, 'r') as f:
+      return f.read()
+
+
+  def get_val(self):
+    val_path = '/content/drive/My Drive/CS_4395_Assignments/A1/val.txt'
+    with open(val_path, 'r') as f:
+      return f.read()
+
+data = Data()
+
+train_data = data.get_train()
+val_data = data.get_val()
+
+
+    
 def preprocess_line(line, lowercase=True):
     tokens = line.strip().split()
     
