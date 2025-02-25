4cd960018a308557435b98929cd55d75e2af66b7
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-25 16:37:00 -0600

    Making the data class generic

--- data.py
+++ data.py
@@ -1,13 +1,21 @@
+class Data:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def get_contents(self) -> str:
+        with open(self.file_path, 'r') as file:
+            return file.read()
+'''
 TRAIN_PATH = './A1_DATASET/train.txt'
 VAL_PATH = './A1_DATASET/val.txt'
 class Data():
     def get_train(self):
         with open(TRAIN_PATH) as file:
             data = file.read()
-        print(data)
         return data
 
     def get_val(self):
         with open(VAL_PATH) as file:
             data = file.read()
         return data
+'''
